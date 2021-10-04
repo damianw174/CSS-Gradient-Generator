a2ab54a292851de9ebfe310ef081672f893578f1
damianw174 <damianw174@wp.pl>
2021-10-04 11:07:30 +0200

    created color blocks

---
+++ index.js
@@ -0,0 +1,17 @@
+var colorBlock1 = {
+    color:document.getElementById("color1"),
+    range:document.getElementById("range1"),
+    number:document.getElementById("number1"),
+}
+
+var colorBlock2 = {
+    color:document.getElementById("color1"),
+    range:document.getElementById("range1"),
+    number:document.getElementById("number1"),
+}
+
+var colorBlock2 = {
+    color:document.getElementById("color3"),
+    range:document.getElementById("range3"),
+    number:document.getElementById("number3"),
+}
