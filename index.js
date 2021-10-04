87f6760543f32dddd81a02046285feb3adbcd786
damianw174 <damianw174@wp.pl>
2021-10-04 13:37:50 +0200

    finish color picker

--- index.js
+++ index.js
@@ -4,8 +4,11 @@
     var G = document.getElementById("range2").value
     var B = document.getElementById("range3").value
     var palete = document.getElementById("colorPalete").style;
+    var cssCode = document.getElementById("cssCode");
 
     palete.backgroundColor = "rgb("+R+","+G+","+B+")";
+
+    cssCode.value = "background-color:rgb("+R+","+G+","+B+")";
     
  }
 
